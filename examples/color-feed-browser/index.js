a311c26927b70a48164b7e928a27fa13d9d432bf
Wouter Verweirder <wouter.verweirder@gmail.com>
2014-12-16 16:40:45 +0100

    compress color feed using zlib

--- examples/color-feed-browser/index.js
+++ examples/color-feed-browser/index.js
@@ -2,7 +2,8 @@ var Kinect2 = require('../../kinect2'),
 	express = require('express'),
 	app = express(),
 	server = require('http').createServer(app),
-	io = require('socket.io').listen(server);
+	io = require('socket.io').listen(server),
+	zlib = require('zlib');
 
 var kinect = new Kinect2();
 
@@ -17,28 +18,49 @@ if(kinect.open()) {
 
 	app.use(express.static(__dirname + '/public'));
 
-	var origLength = 4 * 1920 * 1080;
-	var resizedLength = 4 * 192 * 108;
+	// compression is used as a factor to resize the image
+	// the higher this number, the smaller the image
+	// make sure that the width and height (1920 x 1080) are dividable by this number
+	// also make sure the canvas size in the html matches the resized size
+	var compression = 3;
+
+	var origWidth = 1920;
+	var origHeight = 1080;
+	var origLength = 4 * origWidth * origHeight;
+	var compressedWidth = origWidth / compression;
+	var compressedHeight = origHeight / compression;
+	var resizedLength = 4 * compressedWidth * compressedHeight;
 	//we will send a smaller image (1 / 10th size) over the network
 	var resizedBuffer = new Buffer(resizedLength);
+	var compressing = false;
 	kinect.on('colorFrame', function(data){
-		//data is HD bitmap image, which is a bit too heavy to handle in our browser
-		//only send every 10 pixels over to the browser
-		var y2 = 0;
-		for(var y = 0; y < 1080; y+=10) {
-			y2++;
-			var x2 = 0;
-			for(var x = 0; x < 1920; x+=10) {
-				var i = 4 * (y * 1920 + x);
-				var j = 4 * (y2 * 192 + x2);
-				resizedBuffer[j] = data[i];
-				resizedBuffer[j+1] = data[i+1];
-				resizedBuffer[j+2] = data[i+2];
-				resizedBuffer[j+3] = data[i+3];
-				x2++;
+		//compress the depth data using zlib
+		if(!compressing) {
+			compressing = true;
+			//data is HD bitmap image, which is a bit too heavy to handle in our browser
+			//only send every x pixels over to the browser
+			var y2 = 0;
+			for(var y = 0; y < origHeight; y+=compression) {
+				y2++;
+				var x2 = 0;
+				for(var x = 0; x < origWidth; x+=compression) {
+					var i = 4 * (y * origWidth + x);
+					var j = 4 * (y2 * compressedWidth + x2);
+					resizedBuffer[j] = data[i];
+					resizedBuffer[j+1] = data[i+1];
+					resizedBuffer[j+2] = data[i+2];
+					resizedBuffer[j+3] = data[i+3];
+					x2++;
+				}
 			}
+
+			zlib.deflate(resizedBuffer, function(err, result){
+				if(!err) {
+					io.sockets.emit('colorFrame', result.toString('base64'));
+				}
+				compressing = false;
+			});
 		}
-		io.sockets.emit('colorFrame', resizedBuffer);
 	});
 
 	kinect.openColorReader();
