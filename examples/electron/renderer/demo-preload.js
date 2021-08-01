251cd94a094a0a9952ee0e4c603813a81f262f30
Wouter Verweirder <wouter.verweirder@gmail.com>
2021-08-01 15:03:39 +0200

    updated examples to electron 9.4.x

--- examples/electron/renderer/demo-preload.js
+++ examples/electron/renderer/demo-preload.js
@@ -1,11 +1,18 @@
 const { ipcRenderer } = require('electron');
+
 ipcRenderer.on('close-kinect', (event, message) => {
   if (window.kinect) {
     // properly close the kinect and send a message back to the renderer
+    console.log('close the kinect');
     window.kinect.close().then(() => {
+      console.log('kinect closed');
+    }).catch(e => {
+      console.error(e);
+    }).then(() => {
       event.sender.send('kinect closed');
     });
   } else {
+    console.log("no kinect exposed");
     event.sender.send('kinect closed');
   }
 });
